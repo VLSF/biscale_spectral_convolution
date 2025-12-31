94b4d9c3d71a37bee5c072fa97d16290e7de125b
vlsf <fanaskov.vladimir@gmail.com>
2025-12-31 08:00:38 +0300

    vmap dataset generation

--- datasets/KdV.py
+++ datasets/KdV.py
@@ -3,9 +3,10 @@
 
 import sys
 import jax.numpy as jnp
+import time
 
 from jax.lax import scan
-from jax import random
+from jax import random, vmap
 
 def F(u, delta, dx):
     res = -(jnp.roll(u, 1) + jnp.roll(u, -1) + u) * (jnp.roll(u, -1) - jnp.roll(u, 1))/(6*dx)
@@ -15,6 +16,11 @@ def F(u, delta, dx):
 def integration_step(carry, t):
     u_current, u_prev, dt, dx, delta = carry
     u_next = u_prev + 2*dt*F(u_current, delta, dx)
+    return [u_next, u_current, dt, dx, delta], u_next
+
+def integration_step_vmap(carry, t):
+    u_current, u_prev, dt, dx, delta = carry
+    u_next = u_prev + 2*dt*vmap(F, in_axes=(0, None, None))(u_current, delta, dx)
     return [u_next, u_current, dt, dx, delta], u_next
 
 def RK4_init(u, dt, dx, delta):
@@ -39,7 +45,8 @@ def get_initial_conditions(key, w, N):
     f = f / jnp.linalg.norm(f, ord=jnp.inf)
     return f
 
-def generate_dataset(key, N_samples):
+def generate_dataset(key, N_samples, save_logs_to):
+    N_batch = 25
     N_x = 512
     N_t = 2**20
     x = jnp.linspace(0, 1, N_x+1)[:-1]
@@ -52,26 +59,37 @@ def generate_dataset(key, N_samples):
     alpha = 200
     w = get_weights(N_x, n, alpha)
 
-    keys = random.split(key, N_samples)
+    keys = random.split(key, (N_samples//N_batch, N_batch))
     data = {
         "solutions": [],
         "x": x,
         "t": t[::2**(11)]
     }
+
+    j = 1
     for key in keys:
-        u_prev = get_initial_conditions(key, w, N_x)
-        u_current = RK4_init(u_prev, dt, dx, delta)
+        start = time.time()
+        u_prev = vmap(get_initial_conditions, in_axes=(0, None, None))(key, w, N_x)
+        u_current = vmap(RK4_init, in_axes=(0, None, None, None))(u_prev, dt, dx, delta)
         carry = [u_current, u_prev, dt, dx, delta]
-        _, sol = scan(integration_step, carry, t)
-        sol = jnp.concatenate([u_prev.reshape(1, -1), u_current.reshape(1, -1), sol], axis=0)
-        data["solutions"].append(sol[::2**(11)])
+        _, sol = scan(integration_step_vmap, carry, t)
+        sol = jnp.concatenate([u_prev.reshape(1, N_batch, -1), sol[(2**11-2)::2**11]], axis=0)
+        sol = jnp.transpose(sol, (1, 0, 2))
+        data["solutions"].append(sol)
+        stop = time.time()
+        elapsed_time = stop - start
+        with open(save_logs_to, "a+") as f:
+            f.write(f"\nround {j}/{N_samples//N_batch}, elapsed_time {elapsed_time}")
+        j += 1
+            
         
-    data["solutions"] = jnp.array(data["solutions"])
+    data["solutions"] = jnp.concatenate(data["solutions"], axis=0)
     return data
 
 if __name__ == "__main__":
     dataset_path = sys.argv[1]
+    save_logs_to = sys.argv[2]
     key = random.PRNGKey(444111)
-    N_samples = 1000
-    data = generate_dataset(key, N_samples)
+    N_samples = 25
+    data = generate_dataset(key, N_samples, save_logs_to)
     jnp.savez(dataset_path, **data)
