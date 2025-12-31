043fca30247c0d31dfe6d26e8c75549b6e0b92d0
AndreiChertkov <andre.chertkov@gmail.com>
2025-12-31 11:47:34 +0300

    simplified d=1 KdV dataset

--- datasets/split_KdV_dataset.py
+++ datasets/split_KdV_dataset.py
@@ -4,7 +4,8 @@ import numpy as np
 if __name__ == "__main__":
     kdv_path = sys.argv[1]
     save_d1_path = sys.argv[2]
-    save_d2_path = sys.argv[3]
+    save_d1_path_simplified = sys.argv[3]
+    save_d2_path = sys.argv[4]
     data = np.load(kdv_path)
     d1_data = {
         "features": data['solutions'][:, :1],
@@ -13,6 +14,15 @@ if __name__ == "__main__":
     }
     np.savez(save_d1_path, **d1_data)
     del d1_data
+
+    d1_data = {
+        "features": data['solutions'][:, :1],
+        "targets": data['solutions'][:, ::2][:, 127:128],
+        "coordinates": np.expand_dims(data['x'], 0)
+    }
+    np.savez(save_d1_path_simplified, **d1_data)
+    del d1_data
+
 
     d2_data = {
         "features": np.expand_dims(np.stack([data['solutions'][:, 0, ::2],]*(data['solutions'].shape[-1]//2), axis=1), 1),
