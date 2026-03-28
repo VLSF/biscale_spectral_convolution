51906426fe24d1036691a48d7efc6344d9cbc45b
vlsf <fanaskov.vladimir@gmail.com>
2026-03-29 01:10:20 +0300

    add GKN-FNO with training

---
+++ architectures/GKN_FNO.py
@@ -0,0 +1,161 @@
+import jax.numpy as jnp
+import equinox as eqx
+
+from jax.lax import dot_general, scan
+from jax import random, vmap
+from jax.nn import gelu
+
+def normalize_conv(A, s1=1.0, s2=1.0):
+    A = eqx.tree_at(lambda x: x.weight, A, A.weight * s1)
+    try:
+        A = eqx.tree_at(lambda x: x.bias, A, A.bias * s2)
+    except:
+        pass
+    return A
+
+def rfft2_truncate(inp, M):
+    inp = jnp.fft.rfft2(inp, norm='forward')[:, :, :M]
+    inp = jnp.roll(inp, M//2, axis=1)[:, :M]
+    return inp
+
+def irfft2_pad(inp, M, s):
+    inp = jnp.pad(inp, [(0, 0),] +[(0, s_-inp.shape[i+1]) for i, s_ in enumerate(s)])
+    inp = jnp.roll(inp, -M//2, axis=1)
+    inp = jnp.fft.irfft2(inp, s=s, norm='forward')
+    return inp
+
+def rfft3_truncate(inp, M):
+    inp = jnp.fft.rfftn(inp, axes=(1, 2, 3), norm='forward')[:, :, :, :M]
+    inp = jnp.roll(inp, M//2, axis=1)[:, :M]
+    inp = jnp.roll(inp, M//2, axis=2)[:, :, :M]
+    return inp
+
+def irfft3_pad(inp, M, s):
+    inp = jnp.pad(inp, [(0, 0),] +[(0, s_-inp.shape[i+1]) for i, s_ in enumerate(s)])
+    inp = jnp.roll(inp, -M//2, axis=1)
+    inp = jnp.roll(inp, -M//2, axis=2)
+    inp = jnp.fft.irfftn(inp, axes=(1, 2, 3), s=s, norm='forward')
+    return inp
+
+def get_neighbours(carry, ind, N_max):
+    x0, x1, R = carry
+    point_a = x0[:, ind].reshape(x0.shape[0], 1)
+    distances = jnp.linalg.norm(x1 - point_a, axis=0)
+    mask = (distances <= R)
+    indices_1 = jnp.arange(x1.shape[1])*mask - jnp.logical_not(mask)
+    indices_1 = jnp.sort(indices_1)[-N_max:]
+    indices_0 = jnp.array([ind,]*indices_1.shape[0])
+    return carry, [indices_0, indices_1]
+
+def get_indices(x0, x1, R):
+    D = x0.shape[0]
+    x0 = x0.reshape(x0.shape[0], -1)
+    x1 = x1.reshape(x1.shape[0], -1)
+    h = min(jnp.sort(jnp.sum(x0, axis=0))[1], jnp.sort(jnp.sum(x1, axis=0))[1])
+    N_max = int((2*R / (h))**D)
+    get_neighbours_ = lambda a, b: get_neighbours(a, b, N_max)
+    ind = jnp.arange(x0.shape[1])
+    carry = [x0, x1, R]
+    _, [ind_0, ind_1] = scan(get_neighbours_, carry, ind) 
+    ind_0 = ind_0.reshape(-1,)
+    ind_1 = ind_1.reshape(-1,)
+    ind_0 = ind_0[ind_1 >= 0]
+    ind_1 = ind_1[ind_1 >= 0]
+    return ind_0, ind_1
+
+class GKN(eqx.Module):
+    convs: list
+
+    def __init__(self, N_features, N_layers, key, s1=1.0, s2=1.0):
+        N_in, N_processor = N_features
+        n_f = [N_in,] + [N_processor,]*N_layers + [1]
+        keys = random.split(key, len(n_f)-1)
+        self.convs = [normalize_conv(eqx.nn.Conv(1, n_in, n_out, 1, key=key), s1=s1, s2=s2) for n_in, n_out, key in zip(n_f[:-1], n_f[1:], keys)]
+
+    def __call__(self, u, x0, x1, ind0, ind1):
+        s = x1.shape[1:]
+        u_ = u.reshape(u.shape[0], -1)
+        x0_ = x0.reshape(x0.shape[0], -1)
+        x1_ = x1.reshape(x1.shape[0], -1)
+        w = jnp.concatenate([u_[:, ind0], u_[:, ind1], x0_[:, ind0], x1_[:, ind0]], axis=0)
+        w = self.convs[0](w)
+        for c in self.convs[1:]:
+            w = gelu(w)
+            w = c(w)
+        output = jnp.zeros([u_.shape[0], x1_.shape[1]])
+        output = output.at[:, ind1].add(w*u_[:, ind0])
+        output = output.reshape([u_.shape[0],] + list(s))
+        return output
+        
+class custom_Identity(eqx.Module):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, inp, x0, x1, ind0, ind1):
+        return inp
+
+class GKN_FNO(eqx.Module):
+    encoder: eqx.Module
+    kernel_encoder: eqx.Module
+    kernel_decoder: eqx.Module
+    decoder: eqx.Module
+    convs1: list
+    convs2: list
+    A: jnp.array
+
+    def __init__(self, N_layers, N_features, N_modes, D, J0, J1, kernel_encoder, GKN_proc, GKN_layers, key, s1=1.0, s2=1.0, s3=1.0):
+        # if kernel_encoder == True, resolution changes before encoder, otherwise it changes after decoder
+        n_in, n_processor, n_out = N_features
+        keys = random.split(key, 4 + 2*N_layers)
+        self.encoder = normalize_conv(eqx.nn.Conv(D, n_in, n_processor, 1, key=keys[-1]), s1=s1, s2=s2)
+        self.decoder = normalize_conv(eqx.nn.Conv(D, n_processor, n_out, 1, key=keys[-2]), s1=s1, s2=s2)
+        self.convs1 = [normalize_conv(eqx.nn.Conv(D, n_processor, n_processor, 1, key=key), s1=s1, s2=s2) for key in keys[:N_layers]]
+        self.convs2 = [normalize_conv(eqx.nn.Conv(D, n_processor, n_processor, 1, key=key), s1=s1, s2=s2) for key in keys[N_layers:2*N_layers]]
+        self.A = random.normal(keys[-3], [N_layers, n_processor, n_processor] + [N_modes,]*D, dtype=jnp.complex64) * s3
+        if kernel_encoder:
+            N_features = [2*n_in + 2*D, GKN_proc]
+            self.kernel_decoder = custom_Identity()
+            self.kernel_encoder = GKN(N_features, GKN_layers, keys[-4], s1=s1, s2=s2)
+        else:
+            N_features = [2*n_out + 2*D, GKN_proc]
+            self.kernel_encoder = custom_Identity()
+            self.kernel_decoder = GKN(N_features, GKN_layers, keys[-4], s1=s1, s2=s2)
+    
+    def __call__(self, u, x0, x1, ind0, ind1):
+        u = jnp.concatenate([x0, u], 0)
+        u = self.kernel_encoder(u, x0, x1, ind0, ind1)
+        u = self.encoder(u)
+        for i in range(self.A.shape[0]):
+            u += gelu(self.convs2[i](gelu(self.convs1[i](self.spectral_conv(u, self.A[i])))))
+        u = self.decoder(u)
+        u = self.kernel_decoder(u, x0, x1, ind0, ind1)
+        return u
+
+    def spectral_conv(self, v, A):
+        D = v.ndim - 1
+        if D == 1:
+            u = self.spectral_conv_d1(v, A)
+        elif D == 2:
+            u = self.spectral_conv_d2(v, A)
+        else:
+            u = self.spectral_conv_d3(v, A)
+        return u
+
+    def spectral_conv_d1(self, inp, A):
+        coeff = jnp.fft.rfft(inp, norm='forward', axis=1)[:, :A.shape[2]]
+        coeff = dot_general(A, coeff, (((1,), (0,)), ((2,), (1,)))).T
+        out = jnp.fft.irfft(coeff, norm='forward', n=inp.shape[1], axis=1)
+        return out
+    
+    def spectral_conv_d2(self, inp, A):
+        coeff = rfft2_truncate(inp, A.shape[2])
+        coeff = jnp.moveaxis(dot_general(A, coeff, (((1,), (0,)), ((2, 3), (1, 2)))), 2, 0)
+        out = irfft2_pad(coeff, A.shape[2], inp.shape[1:])
+        return out
+
+    def spectral_conv_d3(self, inp, A):
+        coeff = rfft3_truncate(inp, A.shape[-1])
+        coeff = jnp.moveaxis(dot_general(A, coeff, (((1,), (0,)), ((2, 3, 4), (1, 2, 3)))), 3, 0)
+        out = irfft3_pad(coeff, A.shape[-1], inp.shape[1:])
+        return out
---
+++ scripts/GKN_FNO_train.py
@@ -0,0 +1,290 @@
+import os
+import time
+import jax.numpy as jnp
+import numpy as np
+import equinox as eqx
+import optax
+import hashlib
+import argparse
+import jax
+
+from jax import random, vmap
+from jax.lax import scan
+from jax.tree_util import tree_map, tree_flatten
+from architectures import GKN_FNO
+
+def subsample_field(field, J, D):
+    if D == 1:
+        field = field[:, :, ::2**J]
+    elif D == 2:
+        field = field[:, :, ::2**J, ::2**J]
+    else:
+        field = field[:, :, ::2**J, ::2**J, ::2**J]
+    return jnp.array(field)
+
+def normalise_field(field, D):
+    if D == 1:
+        axis = [0, 2]
+    elif D == 2:
+        axis = [0, 2, 3]
+    else:
+        axis = [0, 2, 3, 4]
+    norm_factor = jnp.max(jnp.abs(field), axis=axis, keepdims=True)
+    norm_factor = norm_factor + (norm_factor == 0)
+    field = field / norm_factor
+    return field
+
+def get_coordinates(N_x, D):
+    x = jnp.linspace(0, 1, N_x)
+    coordinates = jnp.stack(jnp.meshgrid(*[x for _ in range(D)]))
+    return coordinates
+
+def compute_errors(carry, ind, ind0, ind1):
+    model, features, x0, x1, targets = carry
+    prediction = model(features[ind], x0, x1, ind0, ind1)
+    error = jnp.linalg.norm((prediction - targets[ind]).reshape(prediction.shape[0], -1)) / jnp.linalg.norm(targets[ind].reshape(prediction.shape[0], -1))
+    return carry, error
+
+def l2_loss(model, feature, x0, x1, target, ind0, ind1):
+    X = model(feature, x0, x1, ind0, ind1)
+    error = jnp.sum((X - target)**2)
+    return error
+
+def batch_l2_loss(model, feature, x0, x1, target, ind0, ind1):
+    res = vmap(l2_loss, in_axes=(None, 0, None, None, 0, None, None))(model, feature, x0, x1, target, ind0, ind1)
+    return jnp.mean(res)
+
+compute_loss_and_grads = eqx.filter_value_and_grad(batch_l2_loss)
+
+def make_step_scan(carry, n, optim, ind0, ind1):
+    model, features, x0, x1, targets, opt_state = carry
+    loss, grads = compute_loss_and_grads(model, features[n], x0, x1, targets[n], ind0, ind1)
+    grads = tree_map(lambda x: x.conj(), grads)
+    updates, opt_state = optim.update(grads, opt_state, eqx.filter(model, eqx.is_array))
+    model = eqx.apply_updates(model, updates)
+    return [model, features, x0, x1, targets, opt_state], loss
+
+def get_argparser():
+    parser = argparse.ArgumentParser()
+    args = {
+       "-dataset_path": {
+            "help": "absolute path to dataset"
+        },
+       "-results_path": {
+            "help": "absolute path to folder where results are stored"
+        },       
+        "-learning_rate": {
+            "default": 1e-4,
+            "type": float,
+            "help": "learning rate"
+        },
+        "-s1": {
+            "default": 1e-2,
+            "type": float,
+            "help": "normalisation factor for convolutions"
+        },
+        "-s3": {
+            "default": 1e-2,
+            "type": float,
+            "help": "normalisation factor for the Fourier kernel"
+        },
+        "-gamma": {
+            "default": 0.5,
+            "type": float,
+            "help": "decay parameter for the exponential decay of learning rate per N_drop epochs"
+        },
+        "-N_drop": {
+            "default": 10000,
+            "type": int,
+            "help": "multiply learning rate by gamma each N_drop epoch"
+        },
+        "-N_batch": {
+            "default": 10,
+            "type": int,
+            "help": "number of samples used to average gradient"
+        },
+        "-N_train": {
+            "default": 800,
+            "type": int,
+            "help": "number of samples in the training set"
+        },
+        "-N_val": {
+            "default": 100,
+            "type": int,
+            "help": "number of samples in the validation set"
+        },
+        "-N_test": {
+            "default": 100,
+            "type": int,
+            "help": "number of samples in the test set"
+        },
+        "-N_epoch": {
+            "default": 500,
+            "type": int,
+            "help": "number of updates of the model weights = N_epoch * N_train // N_batch"
+        },
+        "-stop_each": {
+            "default": 50,
+            "type": int,
+            "help": "stop each N_epoch to evaluate the model and make checkpoint"
+        },
+        "-N_layers": {
+            "default": 4,
+            "type": int,
+            "help": "number of layers in MLP"
+        },
+        "-N_processor": {
+            "default": 32,
+            "type": int,
+            "help": "number of features in processor"
+        },
+        "-N_modes": {
+            "default": 16,
+            "type": int,
+            "help": "number of modes in joint FNO kernel"
+        },
+        "-J_in": {
+            "default": 0,
+            "type": int,
+            "help": "subsampling rate for each spatial dimension of the input, e.g., x[:, ::2**J]"
+        },
+        "-J_out": {
+            "default": 0,
+            "type": int,
+            "help": "subsampling rate for each spatial dimension of the output, e.g., x[:, ::2**J]"
+        },
+        "-kernel_encoder": {
+            "default": 1,
+            "type": int,
+            "choices": [0, 1],
+            "help": "if 1 kernel encoder is present, otherwise kernel decoder is present"
+        },
+        "-GKN_proc": {
+            "default": 32,
+            "type": int,
+            "help": "number of features in graph kernel network"
+        },
+        "-GKN_layers": {
+            "default": 4,
+            "type": float,
+            "help": "radius of GKN for building KNN graph"
+        },
+        "-GKN_R": {
+            "default": 0.25,
+            "type": int,
+            "help": "number of layers in graph kernel network"
+        },
+        "-key": {
+            "default": 14,
+            "type": int,
+            "help": "PRNGKey that seed all randomness in the code"
+        },
+        "-optim": {
+            "default": 'adam',
+            "type": str,
+            "choices": ["lion", "adam"],
+            "help": "optimiser"
+        }
+    }
+
+    for key in args:
+        parser.add_argument(key, **args[key])
+
+    return parser
+
+if __name__ == "__main__":
+    parser = get_argparser()
+    args = vars(parser.parse_args())
+    exp_hash = "".join([str(args[a]) for a in sorted(args)])
+    exp_hash = hashlib.sha256(str.encode(exp_hash)).hexdigest()
+    dataset_name = args['dataset_path'].split("/")[-1]
+    header = ",".join([key for key in args.keys()])
+    header += ",hash,final_loss,model_size,training_time,train_error,test_error,val_error,best_n"
+
+    if not os.path.isfile(f'{args["results_path"]}/results.csv'):
+        with open(f'{args["results_path"]}/results.csv', "w") as f:
+            f.write(header)
+    
+    key = random.PRNGKey(args["key"])
+    keys = random.split(key, 3)
+    data = jnp.load(args['dataset_path'])
+    targets = data['targets']
+    D = targets.ndim - 2
+    targets = normalise_field(subsample_field(targets, args['J_out'], D), D)
+    J1 = jnp.log2(targets.shape[-1]).astype(int).item()
+    perm = random.permutation(keys[0], targets.shape[0])
+    targets = targets[perm]
+    
+    features = normalise_field(subsample_field(data['features'], args['J_in'], D), D)[perm]
+    x0 = subsample_field(np.expand_dims(data['coordinates'], 0), args['J_in'], D)[0]
+    x1 = subsample_field(np.expand_dims(data['coordinates'], 0), args['J_out'], D)[0]
+    J0 = jnp.log2(features.shape[-1]).astype(int).item()
+
+    ind0, ind1 = GKN_FNO.get_indices(x0, x1, args['GKN_R'])
+
+    N_run = args["N_epoch"] * args["N_train"] // args["N_batch"]
+    N_drop = args["N_drop"] * args["N_train"] // args["N_batch"]
+    N_stop = args["stop_each"] * args["N_train"] // args["N_batch"]
+    
+    N_layers = args["N_layers"]
+    N_features = [x0.shape[0] + features.shape[1], args['N_processor'], targets.shape[1]]
+    N_modes = args["N_modes"]
+    model = GKN_FNO.GKN_FNO(N_layers, N_features, N_modes, D, J0, J1, args['kernel_encoder'] == 1, args['GKN_proc'], args['GKN_layers'], keys[1], s1=args['s1'], s2=0, s3=args['s3'])
+    
+    model_size = sum(jax.tree.map(lambda x: (2*jnp.size(x) if x.dtype == jnp.complex64 else jnp.size(x)) if not (x is None) else 0, jax.tree.flatten(model)[0], is_leaf=eqx.is_array))
+    learning_rate = optax.exponential_decay(args["learning_rate"], N_drop, args["gamma"])
+    if args["optim"] == "lion":
+        optim = optax.lion(learning_rate=learning_rate)
+    else:
+        optim = optax.adam(learning_rate=learning_rate)
+    opt_state = optim.init(eqx.filter(model, eqx.is_array))
+    
+    nn = random.choice(keys[2], args["N_train"], shape = (N_run//N_stop, N_stop, args["N_batch"]))
+    val_ind = args["N_train"] + jnp.arange(args["N_val"])
+    train_ind = jnp.arange(args["N_train"])
+    test_ind = -(1 + jnp.arange(args["N_test"]))
+    carry = [model, features, x0, x1, targets, opt_state]
+    make_step_scan_ = lambda a, b: make_step_scan(a, b, optim, ind0, ind1)
+    compute_errors_ = lambda a, b: compute_errors(a, b, ind0, ind1)
+
+    training_time = 0
+    models = []
+    opt_states = []
+    val_rel_errors = []
+    training_times = []
+    histories = []
+    for nn_ in nn:
+        start = time.time()
+        carry, history = scan(make_step_scan_, carry, nn_)
+        stop = time.time()
+        training_time = training_time + stop - start
+        model = carry[0]
+        opt_state = carry[-1]
+        models.append(model)
+        opt_states.append(opt_state)
+        rel_errors = scan(compute_errors_, [model, features, x0, x1, targets], val_ind)[1]
+        val_rel_errors.append(jnp.mean(rel_errors))
+        training_times.append(training_time)
+        histories.append(history)
+        if jnp.isnan(val_rel_errors[-1]).item():
+            break
+        
+    val_rel_errors = jnp.array(val_rel_errors)
+    val_rel_errors = jnp.nan_to_num(val_rel_errors, nan=jnp.inf)
+    best_n = jnp.argmin(val_rel_errors)
+    concat_n = min(best_n + 1, len(histories))
+    
+    history = jnp.concatenate(histories[:concat_n])
+    eqx.tree_serialise_leaves(f'{args["results_path"]}/model_{exp_hash}.eqx', models[best_n])
+    eqx.tree_serialise_leaves(f'{args["results_path"]}/opt_state_{exp_hash}.eqx', opt_states[best_n])
+
+    train_rel_errors = jnp.mean(scan(compute_errors_, [models[best_n], features, x0, x1, targets], train_ind)[1])
+    val_rel_errors = jnp.mean(scan(compute_errors_, [models[best_n], features, x0, x1, targets], val_ind)[1])
+    test_rel_errors = jnp.mean(scan(compute_errors_, [models[best_n], features, x0, x1, targets], test_ind)[1])
+    data = "\n" + ",".join([str(args[key]) for key in args.keys()])
+    data += f",{exp_hash},{history[-1]},{model_size},{training_times[best_n]},{train_rel_errors},{test_rel_errors},{val_rel_errors},{best_n}"
+                                
+    with open(f'{args["results_path"]}/results.csv', "a") as f:
+        f.write(data)
+                                
+    jnp.savez(f'{args["results_path"]}/metrics_{exp_hash}.npz', rel_errors=rel_errors, history=history, permutation=perm)
