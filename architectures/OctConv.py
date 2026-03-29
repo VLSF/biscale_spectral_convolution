4fb88b8673ec2ceec2e955f22cf94d93b854003d
vlsf <fanaskov.vladimir@gmail.com>
2026-03-29 17:59:00 +0300

    change to dilated conv block

--- architectures/OctConv.py
+++ architectures/OctConv.py
@@ -15,16 +15,25 @@ def normalize_conv(A, s1=1.0, s2=1.0):
         pass
     return A
 
+class DilatedBlock(eqx.Module):
+    convs: list
+
+    def __init__(self, n_processor, D, kernel_size, key, s1=1.0, s2=1.0):
+        dilations = [1, 2, 3, 5, 7, 11, 1]
+        keys = random.split(key, len(dilations))
+        self.convs = [normalize_conv(eqx.nn.Conv(D, n_processor, n_processor, kernel_size, padding=(kernel_size-1)*d//2, dilation=d, key=key), s1=s1, s2=s2) for key, d in zip(keys, dilations)]
+
+    def __call__(self, u):
+        for c in self.convs:
+            u = gelu(c(u))
+        return u
+
 class OctConv(eqx.Module):
     encoder_a: eqx.Module
     encoder_b: eqx.Module
     decoder_a: eqx.Module
-    convs1_a: list
-    convs2_a: list
-    convs3_a: list
-    convs1_b: list
-    convs2_b: list
-    convs3_b: list
+    convs_a: list
+    convs_b: list
     a_to_b: list
     b_to_a: list
 
@@ -37,18 +46,15 @@ class OctConv(eqx.Module):
         self.encoder_b = normalize_conv(eqx.nn.Conv(D, n_in_b, n_processor_b, 1, key=keys[1]), s1=s1, s2=s2)
         self.decoder_a = normalize_conv(eqx.nn.Conv(D, n_processor_a, n_out_a, 1, key=keys[2]), s1=s1, s2=s2)
         
-        keys = random.split(keys[3], 3*N_layers + 1)
-        self.convs1_a = [normalize_conv(eqx.nn.Conv(D, n_processor_a, n_processor_a, kernel_size, padding=kernel_size//2, key=key), s1=s1, s2=s2) for key in keys[:N_layers]]
-        self.convs2_a = [normalize_conv(eqx.nn.Conv(D, n_processor_a, n_processor_a, kernel_size, padding=kernel_size//2, key=key), s1=s1, s2=s2) for key in keys[N_layers:2*N_layers]]
-        self.convs3_a = [normalize_conv(eqx.nn.Conv(D, n_processor_a, n_processor_a, kernel_size, padding=kernel_size//2, key=key), s1=s1, s2=s2) for key in keys[2*N_layers:3*N_layers]]
-        
-        keys = random.split(keys[-1], 3*N_layers - 1)
-        self.convs1_b = [normalize_conv(eqx.nn.Conv(D, n_processor_b, n_processor_b, kernel_size, padding=kernel_size//2, key=key), s1=s1, s2=s2) for key in keys[:(N_layers-1)]]
-        self.convs2_b = [normalize_conv(eqx.nn.Conv(D, n_processor_b, n_processor_b, kernel_size, padding=kernel_size//2, key=key), s1=s1, s2=s2) for key in keys[(N_layers-1):(2*N_layers-2)]]
-        self.convs3_b = [normalize_conv(eqx.nn.Conv(D, n_processor_b, n_processor_b, kernel_size, padding=kernel_size//2, key=key), s1=s1, s2=s2) for key in keys[(2*N_layers-2):(3*N_layers-3)]]
+        keys = random.split(keys[3], N_layers + 1)
+        self.convs_a = [DilatedBlock(n_processor_a, D, kernel_size, key, s1=s1, s2=s2) for key in keys[:N_layers]]
+
+        keys = random.split(keys[-1], N_layers - 1)
+        self.convs_b = [DilatedBlock(n_processor_b, D, kernel_size, key, s1=s1, s2=s2) for key in keys[:(N_layers-1)]]
 
         self.b_to_a = [normalize_conv(eqx.nn.ConvTranspose(D, n_processor_b, n_processor_a, kernel_size, padding=kernel_size//2, stride=2**J_d, output_padding=2**J_d-1, key=key), s1=s1, s2=s2) for key in random.split(keys[-2], N_layers)]
         self.a_to_b = [normalize_conv(eqx.nn.Conv(D, n_processor_a, n_processor_b, kernel_size, padding=kernel_size//2, stride=2**J_d, key=key), s1=s1, s2=s2) for key in random.split(keys[-1], N_layers)]
+
         
     def __call__(self, u_a, x_a, u_b, x_b):
         if u_a is None:
@@ -65,9 +71,9 @@ class OctConv(eqx.Module):
 
         for i in range(len(self.a_to_b)):
             u_b_, u_a_ = self.oct_conv(u_a, u_b, self.a_to_b[i], self.b_to_a[i])
-            u_a += gelu(self.convs3_a[i](gelu(self.convs2_a[i](self.convs1_a[i](u_a) + u_a_))))
-            if i != len(self.convs1_b):
-                u_b += gelu(self.convs3_b[i](gelu(self.convs2_b[i](self.convs1_b[i](u_b) + u_b_))))
+            u_a += self.convs_a[i](u_a + u_a_)
+            if i != len(self.convs_b):
+                u_b += self.convs_b[i](u_b + u_b_)
         u_a = self.decoder_a(u_a)
         return u_a, u_b
 
